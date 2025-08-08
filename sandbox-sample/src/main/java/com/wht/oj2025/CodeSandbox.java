d4208243d43673df56c3cecb62ac81858205daf4
Haotian Wu <101766788+WU-HAOTIAN34@users.noreply.github.com>
2025-08-08 13:03:19 +0800

    sand box sample

---
+++ sandbox-sample/src/main/java/com/wht/oj2025/CodeSandbox.java
@@ -0,0 +1,19 @@
+package com.wht.oj2025;
+
+
+import com.yupi.yuojcodesandbox.model.ExecuteCodeRequest;
+import com.yupi.yuojcodesandbox.model.ExecuteCodeResponse;
+
+/**
+ * 代码沙箱接口定义
+ */
+public interface CodeSandbox {
+
+    /**
+     * 执行代码
+     *
+     * @param executeCodeRequest
+     * @return
+     */
+    ExecuteCodeResponse executeCode(ExecuteCodeRequest executeCodeRequest);
+}
